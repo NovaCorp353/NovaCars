6094f0f946899c61c4b6811eb33355dc8590b035
Ani Kristo <anikristo@gmail.com>
2016-04-27 13:19:41 +0300

    PHP for register is ready

---
+++ webpage/js/register.js
@@ -0,0 +1,33 @@
+$("#register-form").submit(function(event){
+	event.preventDefault();
+	var f = $('#inputFirstName').val();
+	var l = $('#inputLastName').val();
+	var e = $('#inputEmail').val();
+	var p = $('#inputPassword').val();
+	if(e != null && e != "" && p != null && p != ""){
+		$.ajax({
+			type: "POST",
+			url: "php/register.php",
+			data: {firstname: f, lastname: l, email:e, password:p},
+			cache: false,
+			beforeSend: function(){ $("#register-btn").val('Connecting...');},
+			success: function(data){
+				alert(data);
+				if(data == 1) {					
+					registered();
+				} else {
+					$('#register-err-lb').text('This email is already registered!');
+					$('#register-err').css('visibility','visible');   
+				}
+			}
+		});
+	} else {
+		$('#register-err-lb').text('All fields are required!');
+		$('#register-err').css('visibility','visible');   
+	}
+});
+
+function registered(){
+	$('#register-err').css('visibility','hidden');   
+	alert("registered");
+}
---
+++ webpage/php/register.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'conn.php';
+
+function registerUser($mysqli){
+	if(!empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["firstname"]) && !empty($_POST["lastname"])){
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$firstname = $_POST["firstname"];
+		$lastname = $_POST["lastname"];
+
+		$query = "INSERT INTO users (email, password, name, surname) VALUES ('$email', '$password', '$firstname', '$lastname');";
+		if ($mysqli->query($query) == TRUE) 
+		{
+			// $_SESSION["user"] = $uname; --> necessary?
+			echo 1;
+		} else 
+		echo 0;
+	}
+}
+
+$conn = openConn();
+$res = registerUser($conn);
+closeConn($conn);
+return $res;
+?>
