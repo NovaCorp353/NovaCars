6b0b85c6ff9c8b36a76ebf9f775acab63ad48770
Ani Kristo <anikristo@gmail.com>
2016-05-03 19:52:13 +0300

    commit from Yasemin: static page update

--- webpage/js/signin.js
+++ webpage/js/signin.js
@@ -1,3 +1,33 @@
 $('#register').click(function(){
 	window.location.href = 'register.html';
 });	
+
+$('#signin-btn').click(function(event) {
+	event.preventDefault();
+	var u = $('#inputEmail').val();
+	var p = $('#inputPassword').val();
+	if(u != null && u != "" && p != null && p != ""){
+		$.ajax({
+			type: "POST",
+			url: "php/login.php",
+			data: {username:u, password:p},
+			cache: false,
+			beforeSend: function(){ $("#signin-btn").val('Connecting...');},
+			success: function(data){
+				if(data == 1) {					
+					loggedin();
+				} else {
+					$('#signin-err').text('Login failed. Check credentials!');
+					$('#signin-err').css('visibility','visible');   
+				}
+			}
+		});
+	} else {
+		$('#signin-err').text('All fields are required!');
+		$('#signin-err').css('visibility','visible');   
+	}
+});
+
+function loggedin(){
+	alert('logged in');
+}
