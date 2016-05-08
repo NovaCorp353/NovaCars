ed5c7fddf75633537513d489ef694d60887789bc
Ani Kristo <anikristo@gmail.com>
2016-05-08 12:02:03 +0300

    REadded the getContent() function

--- webpage/js/dashboard.js
+++ webpage/js/dashboard.js
@@ -29,3 +29,24 @@ $(document).ready(function() {
 		});
 });
 
+function getContent(contentType){
+ 	var content = $('#content');
+ 	$.ajax({
+ 			type: "POST",
+ 			url: "php/dashboard.php",
+ 			data: {action:contentType},
+ 			cache: false,
+ 			beforeSend: function(){content.html('<p>Retrieving data...</p>');},
+ 			success: function(data){
+ 				if(data) {	
+ 					alert(data);
+ 					content.html(data);
+ 				} else {
+ 					// TODO also log out / end session
+ 					$('#signin-err-lb').text('Unable to retrieve data at this time! Try again later.');
+ 					$('#signin-err').css('visibility','visible');   
+ 				}
+ 			}
+ 		});
+ }
+
